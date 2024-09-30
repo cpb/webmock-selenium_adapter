37a82024b07f936fe3d72ad4eee2d407a8b15de2
Caleb Buxton <me@cpb.ca>
2024-09-30 16:25:24 -0700

    🧪 Must not allow unregistered requests

--- spec/webmock/selenium_adapter_spec.rb
+++ spec/webmock/selenium_adapter_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Webmock::SeleniumAdapter do
   it "has a version number" do
     expect(Webmock::SeleniumAdapter::VERSION).not_to be nil
   end
+
+  it "does not allow unregistered requests" do
+    driver = Selenium::WebDriver.for(:chrome)
+
+    expect { driver.navigate.to "https://httpstat.us/200" }.to raise_error(WebMock::NetConnectNotAllowedError)
+
+    drvier.quit
+  end
 end
